441db5d939e5474b4f6a14cffa326c4ad903f1a9
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2023-03-22 11:07:31 -0300

    Update 1-build_new_project.sh

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-dotnet new console --output 1-new_project
-dotnet build 1-new_project
+
+# Create a new directory for the project
+mkdir 1-new_project
+
+# Change into the new directory
+cd 1-new_project
+
+# Initialize a new C# console project using the dotnet command
+dotnet new console
+
+# Build the project using the dotnet command
+dotnet build
