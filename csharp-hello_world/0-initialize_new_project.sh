2b68cce93d5804ac1ca92660e14d6258858452ff
MarceloRB1 <105667144+MarceloRB1@users.noreply.github.com>
2023-03-22 11:00:10 -0300

    Update 0-initialize_new_project.sh

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir 0-new_project    # create a new directory titled "0-new_project"
-cd 0-new_project       # change the current working directory to "0-new_project"
-dotnet new console     # use the "dotnet" command to create a new C# console project
+dotnet new console --output
 
